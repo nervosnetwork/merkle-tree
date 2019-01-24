09f9ef3cfd920c7f5cfaa8a08540054dc63a5a16
Ke Wang <github@bll.io>
2019-01-24 12:10:24 +0800

    add feature

--- src/lib.rs
+++ src/lib.rs
@@ -6,4 +6,33 @@ pub trait Merge {
     fn merge(left: &Self, right: &Self) -> Self;
 }
 
-pub use merkle_tree::new_cbmt;
+#[cfg(feature = "sha3")]
+pub use numext_fixed_hash::H256;
+
+#[cfg(feature = "blake2b")]
+pub use numext_fixed_hash::H256;
+
+#[cfg(feature = "sha3")]
+impl Merge for H256 {
+    fn merge(left: &H256, right: &H256) -> H256 {
+        let mut hash = [0u8; 32];
+        let mut sha3 = tiny_keccak::Keccak::new_sha3_256();
+        sha3.update(left.as_bytes());
+        sha3.update(right.as_bytes());
+        sha3.finalize(&mut hash);
+        hash.into()
+    }
+}
+
+#[cfg(feature = "blake2b")]
+impl Merge for H256 {
+    fn merge(left: &H256, right: &H256) -> H256 {
+        let mut context = blake2_rfc::blake2b::Blake2b::new(64);
+        context.update(left.as_bytes());
+        context.update(right.as_bytes());
+        let hash = context.finalize();
+        hash.as_bytes().into()
+    }
+}
+
+pub use crate::merkle_tree::new_cbmt;
